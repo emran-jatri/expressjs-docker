72cdaa295cc31d9a4d7453b4f572525ecec38256
ibnshayed <ibnshayed@gmail.com>
2022-10-11 09:18:25 +0600

    nodemon and env added

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const app = express();
 
 
 app.get('/', (req, res) => {
-	res.send("<h2>Hi there!!!</h2>")
+	res.send("<h2>Hi there</h2>")
 })
 
 const port = process.env.PORT || 3000;
+console.log("🚀 ~ file: index.js ~ line 11 ~ process.env.PORT", process.env.PORT)
 app.listen(port, () => console.log('listening on http://localhost:'+ port));
