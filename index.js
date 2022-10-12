a5e08bc6808c475db1ab36b11bf1b7ef16e54662
ibnshayed <ibnshayed@gmail.com>
2022-10-12 13:12:45 +0600

    depends on added

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
+const express = require("express");
+const mongoose = require("mongoose");
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_IP,
+  MONGO_PORT,
+} = require("./config/config");
 
 const app = express();
 
-const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
-mongoose
-	// .connect("mongodb://root:example@172.22.0.2:27017/?authSource=admin")
-	// .connect("mongodb://root:example@mongo:27017/?authSource=admin")
-	.connect(mongoURL)
-	.then(() => console.log("Successfully connected to MongoDB"))
-	.catch(error => console.log(error))
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 
-app.get('/', (req, res) => {
-	res.send("<h2>Hi there !!!!</h2>")
-})
+const connectWithRetry = () => {
+  mongoose
+    // .connect("mongodb://root:example@172.22.0.2:27017/?authSource=admin")
+    // .connect("mongodb://root:example@mongo:27017/?authSource=admin")
+    .connect(mongoURL)
+    .then(() => console.log("Successfully connected to MongoDB"))
+		.catch((error) => {
+			console.log(error)
+			setTimeout(connectWithRetry, 5000)
+			} 
+		);
+};
+
+connectWithRetry()
+
+app.get("/", (req, res) => {
+  res.send("<h2>Hi there !!!!</h2>");
+});
 
 const port = process.env.PORT || 3000;
-console.log("🚀 ~ file: index.js ~ line 11 ~ process.env.PORT", process.env.PORT)
-app.listen(port, () => console.log('listening on http://localhost:'+ port));
+console.log(
+  "🚀 ~ file: index.js ~ line 11 ~ process.env.PORT",
+  process.env.PORT
+);
+app.listen(port, () => console.log("listening on http://localhost:" + port));
