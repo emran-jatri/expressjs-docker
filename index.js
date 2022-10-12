cd6c7006ac846b6ef74eee8dd8f85fb94214ddee
emran-jatri <emran.jatri@gmail.com>
2022-10-12 18:08:41 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
 
 const app = express();
 
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+mongoose
+	// .connect("mongodb://root:example@172.22.0.2:27017/?authSource=admin")
+	// .connect("mongodb://root:example@mongo:27017/?authSource=admin")
+	.connect(mongoURL)
+	.then(() => console.log("Successfully connected to MongoDB"))
+	.catch(error => console.log(error))
 
 app.get('/', (req, res) => {
 	res.send("<h2>Hi there !!!!</h2>")
